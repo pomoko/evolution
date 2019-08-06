1c865cde284792d3e9eb1e0ca545e21dac5611bd
pomoko <p.khouzani@gmail.com>
2019-08-06 09:29:22 -0400

    updates

--- resources/views/comments/comments.blade.php
+++ resources/views/comments/comments.blade.php
@@ -128,10 +128,10 @@
       </div><!--row-->
       <hr/>
       <div class="row">
-        <div class="col-xs-12 col-sm-9">
+        <div class="col-xs-5 col-sm-9">
           <h3>همه نظرات ({{convertNumberToPersian(count($comments))}})</h3>
         </div>
-        <div class="col-xs-12 col-sm-3 text-left">
+        <div class="col-xs-7 col-sm-3 text-left">
           <form>
             <div class="form-group text-left">
               <select id="selectOrder" class="form-control">
