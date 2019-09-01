73bd54ffde5a652de1454ca7b468c575500f4a8e
pomoko <p.khouzani@gmail.com>
2019-09-01 05:59:01 -0400

    university

--- resources/views/info/articles/articles.blade.php
+++ resources/views/info/articles/articles.blade.php
@@ -13,11 +13,11 @@
 
             <div class="row">
               <div class="col-xs-12">
-<img src={{asset("img/sampleArticles/40.jpg")}} class="img-responsive center-block img-rounded" alt="Responsive image">
-
+                <img src={{asset("img/sampleArticles/40.jpg")}} class="img-responsive center-block img-rounded" alt="Responsive image">
               </div>
             </div>
-            <div class="row">
+            
+          <!--   <div class="row">
               <div class="col-md-4 col-sm-12"><img src={{asset("img/book.jpg")}} class="img-responsive rounded mx-auto d-block" alt="book"> </div>
               <div class="col-md-8 col-sm-12 text-right"><h3>آیا میدانستید که....؟</h3></div>
             </div>
@@ -55,7 +55,7 @@
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
               </div>
             </div>
-            <hr/>
+            <hr/> -->
 
           </div>
         </div> 
