3c4775c481d59e2e75b3e18c163f0fe52639ddfc
pomoko <p.khouzani@gmail.com>
2019-10-09 04:01:00 -0400

    pdf viewer

--- resources/views/info/preface/preface.blade.php
+++ resources/views/info/preface/preface.blade.php
@@ -7,8 +7,30 @@
     <div class="container">
       <div class="row">
         <div class="text-center col-sm-8 col-sm-offset-2">
-          <h2>پیشگفتار</h2>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+            <div class="row">
+              <div class="col-xs-12">
+                <img src={{asset("img/preface/fa/preface1.jpg")}} class="img-responsive center-block img-rounded" alt="Responsive image">
+              </div>
+            </div>
+
+            <div class="row">
+              <div class="col-xs-12">
+                <img src={{asset("img/preface/fa/preface2.jpg")}} class="img-responsive center-block img-rounded" alt="Responsive image">
+              </div>
+            </div>
+
+            <div class="row">
+              <div class="col-xs-12">
+                <img src={{asset("img/preface/fa/preface3.jpg")}} class="img-responsive center-block img-rounded" alt="Responsive image">
+              </div>
+            </div>
+
+            <div class="row">
+              <div class="col-xs-12">
+                <img src={{asset("img/preface/fa/preface4.jpg")}} class="img-responsive center-block img-rounded" alt="Responsive image">
+              </div>
+            </div>
         </div>
       </div> 
     </div>
