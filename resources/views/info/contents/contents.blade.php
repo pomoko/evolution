93f583ad97e75dd41e45b861bcc6751ef82e0d1e
pomoko <p.khouzani@gmail.com>
2019-10-09 03:56:34 -0400

    pdf viewer

--- resources/views/info/contents/contents.blade.php
+++ resources/views/info/contents/contents.blade.php
@@ -8,8 +8,8 @@
       <div class="heading wow fadeInUp" data-wow-duration="1000ms" data-wow-delay="300ms">
         <div class="row">
           <div class="text-center col-sm-8 col-sm-offset-2">
-            <h2>نمونه مقالات</h2>
-            
+<!--             <h2>نمونه مقالات</h2>
+ -->            
             <div class="row">
               <div class="col-xs-12">
                 <img src={{asset("img/contents/fa/contentsFa1.jpg")}} class="img-responsive center-block img-rounded" alt="Responsive image">
