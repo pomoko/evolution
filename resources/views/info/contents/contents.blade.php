46900ea76f6670b14c3a595080fd37f0014a852a
pomoko <p.khouzani@gmail.com>
2019-10-09 03:55:18 -0400

    pdf viewer

--- resources/views/info/contents/contents.blade.php
+++ resources/views/info/contents/contents.blade.php
@@ -3,19 +3,66 @@
 
 @section('content')
 
-  <section id="contents">
+  <section id="services">
     <div class="container">
-      <div class="row">
-        <div class="text-center col-sm-8 col-sm-offset-2">
-          <h2>فهرست</h2>
-          <ul class="list-group">
-            <li class="list-group-item text-right active"><strong>chapter 1</strong> <span class="badge">page#</span></li>
-            <li class="list-group-item text-right">sub-chapter <span class="badge">page#</span></li> 
-            <li class="list-group-item text-right">sub-chapter <span class="badge">page#</span></li> 
-            <li class="list-group-item text-right active"><strong>chapter 2</strong> <span class="badge">page#</span></li>
-            <li class="list-group-item text-right">sub-chapter <span class="badge">page#</span></li> 
-            <li class="list-group-item text-right">sub-chapter <span class="badge">page#</span></li> 
-          </ul>
+      <div class="heading wow fadeInUp" data-wow-duration="1000ms" data-wow-delay="300ms">
+        <div class="row">
+          <div class="text-center col-sm-8 col-sm-offset-2">
+            <h2>نمونه مقالات</h2>
+            
+            <div class="row">
+              <div class="col-xs-12">
+                <img src={{asset("img/contents/fa/contentsFa1.jpg")}} class="img-responsive center-block img-rounded" alt="Responsive image">
+              </div>
+            </div>
+            
+            <div class="row">
+              <div class="col-xs-12">
+                <img src={{asset("img/contents/fa/contentsFa2.jpg")}} class="img-responsive center-block img-rounded" alt="Responsive image">
+              </div>
+            </div>
+            
+            <div class="row">
+              <div class="col-xs-12">
+                <img src={{asset("img/contents/fa/contentsFa3.jpg")}} class="img-responsive center-block img-rounded" alt="Responsive image">
+              </div>
+            </div>
+            
+            <div class="row">
+              <div class="col-xs-12">
+                <img src={{asset("img/contents/fa/contentsFa4.jpg")}} class="img-responsive center-block img-rounded" alt="Responsive image">
+              </div>
+            </div>
+            
+            <div class="row">
+              <div class="col-xs-12">
+                <img src={{asset("img/contents/fa/contentsFa5.jpg")}} class="img-responsive center-block img-rounded" alt="Responsive image">
+              </div>
+            </div>
+            
+            <div class="row">
+              <div class="col-xs-12">
+                <img src={{asset("img/contents/fa/contentsFa6.jpg")}} class="img-responsive center-block img-rounded" alt="Responsive image">
+              </div>
+            </div>
+            
+            <div class="row">
+              <div class="col-xs-12">
+                <img src={{asset("img/contents/fa/contentsFa7.jpg")}} class="img-responsive center-block img-rounded" alt="Responsive image">
+              </div>
+            </div>
+            
+            <div class="row">
+              <div class="col-xs-12">
+                <img src={{asset("img/contents/fa/contentsFa8.jpg")}} class="img-responsive center-block img-rounded" alt="Responsive image">
+              </div>
+            </div>
+            
+            <div class="row">
+              <div class="col-xs-12">
+                <img src={{asset("img/contents/fa/contentsFa9.jpg")}} class="img-responsive center-block img-rounded" alt="Responsive image">
+              </div>
+            </div>
         </div>
       </div> 
 
