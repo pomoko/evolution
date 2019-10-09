ff0338238c3e4b144482fae98c111ad641ab2f6f
pomoko <p.khouzani@gmail.com>
2019-10-09 03:46:39 -0400

    pdf viewer

--- resources/views/info/articles/articles.blade.php
+++ resources/views/info/articles/articles.blade.php
@@ -74,6 +74,263 @@
               </div>
             </div>
             
+            <hr/>
+
+            <div class="row">
+              <div class="col-xs-12">
+                <img src={{asset("img/sampleArticles/94.jpg")}} class="img-responsive center-block img-rounded" alt="Responsive image">
+              </div>
+            </div>
+            
+            <hr/>
+
+            <div class="row">
+              <div class="col-xs-12">
+                <img src={{asset("img/sampleArticles/95.jpg")}} class="img-responsive center-block img-rounded" alt="Responsive image">
+              </div>
+            </div>
+            
+            <hr/>
+
+            <div class="row">
+              <div class="col-xs-12">
+                <img src={{asset("img/sampleArticles/101.jpg")}} class="img-responsive center-block img-rounded" alt="Responsive image">
+              </div>
+            </div>
+            
+            <hr/>
+
+            <div class="row">
+              <div class="col-xs-12">
+                <img src={{asset("img/sampleArticles/127.jpg")}} class="img-responsive center-block img-rounded" alt="Responsive image">
+              </div>
+            </div>
+            
+            <hr/>
+
+            <div class="row">
+              <div class="col-xs-12">
+                <img src={{asset("img/sampleArticles/128.jpg")}} class="img-responsive center-block img-rounded" alt="Responsive image">
+              </div>
+            </div>
+            
+            <hr/>
+
+            <div class="row">
+              <div class="col-xs-12">
+                <img src={{asset("img/sampleArticles/131.jpg")}} class="img-responsive center-block img-rounded" alt="Responsive image">
+              </div>
+            </div>
+            
+            
+            <hr/>
+
+            <div class="row">
+              <div class="col-xs-12">
+                <img src={{asset("img/sampleArticles/142.jpg")}} class="img-responsive center-block img-rounded" alt="Responsive image">
+              </div>
+            </div>
+            
+            <hr/>
+
+            <div class="row">
+              <div class="col-xs-12">
+                <img src={{asset("img/sampleArticles/144.jpg")}} class="img-responsive center-block img-rounded" alt="Responsive image">
+              </div>
+            </div>
+            
+            <hr/>
+
+            <div class="row">
+              <div class="col-xs-12">
+                <img src={{asset("img/sampleArticles/159.jpg")}} class="img-responsive center-block img-rounded" alt="Responsive image">
+              </div>
+            </div>
+            
+            <hr/>
+
+            <div class="row">
+              <div class="col-xs-12">
+                <img src={{asset("img/sampleArticles/169.jpg")}} class="img-responsive center-block img-rounded" alt="Responsive image">
+              </div>
+            </div>
+            
+            <hr/>
+
+            <div class="row">
+              <div class="col-xs-12">
+                <img src={{asset("img/sampleArticles/182.jpg")}} class="img-responsive center-block img-rounded" alt="Responsive image">
+              </div>
+            </div>
+            
+            <hr/>
+
+            <div class="row">
+              <div class="col-xs-12">
+                <img src={{asset("img/sampleArticles/206.jpg")}} class="img-responsive center-block img-rounded" alt="Responsive image">
+              </div>
+            </div>
+            
+            <hr/>
+
+            <div class="row">
+              <div class="col-xs-12">
+                <img src={{asset("img/sampleArticles/207.jpg")}} class="img-responsive center-block img-rounded" alt="Responsive image">
+              </div>
+            </div>
+            
+            <hr/>
+
+            <div class="row">
+              <div class="col-xs-12">
+                <img src={{asset("img/sampleArticles/209.jpg")}} class="img-responsive center-block img-rounded" alt="Responsive image">
+              </div>
+            </div>
+            
+            <hr/>
+
+            <div class="row">
+              <div class="col-xs-12">
+                <img src={{asset("img/sampleArticles/238.jpg")}} class="img-responsive center-block img-rounded" alt="Responsive image">
+              </div>
+            </div>
+            
+            <hr/>
+
+            <div class="row">
+              <div class="col-xs-12">
+                <img src={{asset("img/sampleArticles/239.jpg")}} class="img-responsive center-block img-rounded" alt="Responsive image">
+              </div>
+            </div>
+            
+            <hr/>
+
+            <div class="row">
+              <div class="col-xs-12">
+                <img src={{asset("img/sampleArticles/248.jpg")}} class="img-responsive center-block img-rounded" alt="Responsive image">
+              </div>
+            </div>
+            
+            <hr/>
+
+            <div class="row">
+              <div class="col-xs-12">
+                <img src={{asset("img/sampleArticles/274.jpg")}} class="img-responsive center-block img-rounded" alt="Responsive image">
+              </div>
+            </div>
+            
+            <hr/>
+
+            <div class="row">
+              <div class="col-xs-12">
+                <img src={{asset("img/sampleArticles/275.jpg")}} class="img-responsive center-block img-rounded" alt="Responsive image">
+              </div>
+            </div>
+            
+            <hr/>
+
+            <div class="row">
+              <div class="col-xs-12">
+                <img src={{asset("img/sampleArticles/286.jpg")}} class="img-responsive center-block img-rounded" alt="Responsive image">
+              </div>
+            </div>
+            
+            <hr/>
+
+            <div class="row">
+              <div class="col-xs-12">
+                <img src={{asset("img/sampleArticles/287.jpg")}} class="img-responsive center-block img-rounded" alt="Responsive image">
+              </div>
+            </div>
+            
+            <hr/>
+
+            <div class="row">
+              <div class="col-xs-12">
+                <img src={{asset("img/sampleArticles/298.jpg")}} class="img-responsive center-block img-rounded" alt="Responsive image">
+              </div>
+            </div>
+            
+            <hr/>
+
+            <div class="row">
+              <div class="col-xs-12">
+                <img src={{asset("img/sampleArticles/319.jpg")}} class="img-responsive center-block img-rounded" alt="Responsive image">
+              </div>
+            </div>
+            
+            <hr/>
+
+            <div class="row">
+              <div class="col-xs-12">
+                <img src={{asset("img/sampleArticles/320.jpg")}} class="img-responsive center-block img-rounded" alt="Responsive image">
+              </div>
+            </div>
+            
+            <hr/>
+
+            <div class="row">
+              <div class="col-xs-12">
+                <img src={{asset("img/sampleArticles/321.jpg")}} class="img-responsive center-block img-rounded" alt="Responsive image">
+              </div>
+            </div>
+            
+            <hr/>
+
+            <div class="row">
+              <div class="col-xs-12">
+                <img src={{asset("img/sampleArticles/327.jpg")}} class="img-responsive center-block img-rounded" alt="Responsive image">
+              </div>
+            </div>
+            
+            <hr/>
+
+            <div class="row">
+              <div class="col-xs-12">
+                <img src={{asset("img/sampleArticles/328.jpg")}} class="img-responsive center-block img-rounded" alt="Responsive image">
+              </div>
+            </div>
+            
+            <hr/>
+
+            <div class="row">
+              <div class="col-xs-12">
+                <img src={{asset("img/sampleArticles/329.jpg")}} class="img-responsive center-block img-rounded" alt="Responsive image">
+              </div>
+            </div>
+            
+            <hr/>
+
+            <div class="row">
+              <div class="col-xs-12">
+                <img src={{asset("img/sampleArticles/340.jpg")}} class="img-responsive center-block img-rounded" alt="Responsive image">
+              </div>
+            </div>
+            
+            <hr/>
+
+            <div class="row">
+              <div class="col-xs-12">
+                <img src={{asset("img/sampleArticles/346.jpg")}} class="img-responsive center-block img-rounded" alt="Responsive image">
+              </div>
+            </div>
+            
+            <hr/>
+
+            <div class="row">
+              <div class="col-xs-12">
+                <img src={{asset("img/sampleArticles/347.jpg")}} class="img-responsive center-block img-rounded" alt="Responsive image">
+              </div>
+            </div>
+            
+            <hr/>
+
+            <div class="row">
+              <div class="col-xs-12">
+                <img src={{asset("img/sampleArticles/365.jpg")}} class="img-responsive center-block img-rounded" alt="Responsive image">
+              </div>
+            </div>
+            
           <!--   <div class="row">
               <div class="col-md-4 col-sm-12"><img src={{asset("img/book.jpg")}} class="img-responsive rounded mx-auto d-block" alt="book"> </div>
               <div class="col-md-8 col-sm-12 text-right"><h3>آیا میدانستید که....؟</h3></div>
