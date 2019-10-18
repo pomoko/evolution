08999bff2d85c145ee5fabec6c4798b654ffdc29
pomoko <p.khouzani@gmail.com>
2019-10-18 07:51:08 -0400

    pdf viewer

--- resources/views/eng/info/message/message.blade.php
+++ resources/views/eng/info/message/message.blade.php
@@ -9,19 +9,19 @@
         <div class="text-center col-sm-8 col-sm-offset-2">
           <h2>Message from the Author</h2>
           <p>Symphony of Evolution is a quarto-sized, 442-page book dealing with the birth
-of life and the evolution of humans.
+of life and the evolution of humans.<br/>
 The 10-chapter book starts with the Big Bang and the birth of the Universe. It then
-continues with the birth of the Milky Way, the Solar System and the Planet Earth.
-The title of this first chapter is “Birth of the Earth”.
+continues with the birth of the Milky Way, the Solar System and the Planet Earth.<br/>
+The title of this first chapter is “Birth of the Earth”.<br/>
 Chapter two, titled “Birth of Life”, deals with the formation of the Moon, the Earth’s
-oceans and life itself.
+oceans and life itself.<br/>
 Chapters three to nine, dealing respectively with the birth of animals, chordates,
 tetrapods, mammals, primates, homos and homo sapiens, explain in detail how
 life forms evolved; how the first cell evolved into animals and how a certain line
-of animals finally evolved into humans.
+of animals finally evolved into humans.<br/>
 The important thing about these chapters is that the evolutionary line that evolved
-into human beings is explained in an uninterrupted, linear manner.
-The final chapter explains how humans conquered the World.
+into human beings is explained in an uninterrupted, linear manner.<br/>
+The final chapter explains how humans conquered the World.<br/>
 Currently, the book is available only in Persian; but an English translation will be
 published in due time.</p>
         </div>
